d2dd0bfef9268e7472ef9d1c8f8ed36a56c3ff00
Vansh Goel <goelvansh82@gmail.com>
2023-06-15 02:24:29 +0530

    Added logic to change state

--- src/Content.js
+++ src/Content.js
@@ -23,6 +23,7 @@ const Content = () => {
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item)
+    setItems(listItems)
   }
     
       return (
