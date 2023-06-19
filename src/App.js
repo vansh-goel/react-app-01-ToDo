fdbf91efa80ffadf64a55f88affe5e31ffea87ad
Vansh Goel <goelvansh82@gmail.com>
2023-06-19 23:24:39 +0530

    Fixed the CSS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
           <Header/>
-        <Content/>
+        <Content className="content"/>
         <Footer/>
       </header>
     </div>
