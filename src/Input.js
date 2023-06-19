99747297ad3163ba5d727434b58f55ba0a70415b
Vansh Goel <goelvansh82@gmail.com>
2023-06-19 22:55:29 +0530

    Added input.js

---
+++ src/Input.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Input = ({ addItem }) => {
+  const [newItem, setNewItem] = useState('');
+
+  const handleAddItem = () => {
+    if (newItem.trim() !== '') {
+      addItem(newItem);
+      setNewItem('');
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+        handleAddItem()
+    }
+  }
+
+  return (
+    <div className='inp'>
+      <input
+        id="input-button" 
+        onKeyDown={handleKeyDown}
+        placeholder='Enter list item'
+        type="text"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default Input;
